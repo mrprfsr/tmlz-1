636589cfb212a7790a0641e2e919f074f8898ca7
mrprfsr <129967425+mrprfsr@users.noreply.github.com>
2023-05-18 01:54:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,8 +30,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://container:container@cluster0.x8edtgx.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cat:cat@cluster0.qptrdyv.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
@@ -70,6 +70,6 @@ if AUTO_DELETE == "True":
     AUTO_DELETE = True
 
 #Sample
-SHORTNER_SITE = "mdiskshortner.link"
-SHORTNER_API = "7aeadf631fe48fce0ed4b480300ee24c89a1b8ae"
+SHORTNER_SITE = "Teraboxshortner.com"
+SHORTNER_API = "ce3983adff187917b6c147cd7f9ed77214580a0b"
 
